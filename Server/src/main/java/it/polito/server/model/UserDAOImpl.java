442e0eec97513f2b578342254ace93c9d00ecbcb
corrado1992 <37770025+corrado1992@users.noreply.github.com>
2018-04-15 11:23:27 +0200

    test findAll()

--- Server/src/main/java/it/polito/server/model/UserDAOImpl.java
+++ Server/src/main/java/it/polito/server/model/UserDAOImpl.java
@@ -1,35 +1,77 @@
 package it.polito.server.model;
 
+import java.sql.*;
 import java.util.List;
 
+import static it.polito.server.model.DbConnection.*;
+
 public class UserDAOImpl implements it.polito.server.model.UserDAO {
+  Connection connection = null;
+  PreparedStatement ps = null;
+  ResultSet resultSet = null;
+
   @Override
   public void insert(User u) {
-
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void delete(User u) {
-
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void update(User u) {
-
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public User findById(int id) {
+
     return null;
   }
 
   @Override
   public List<User> findAll() {
+    try {
+      String queryString = "SELECT * FROM users";
+      connection = getInstance().getConnection();
+      ps = connection.prepareStatement(queryString);
+      resultSet = ps.executeQuery();
+      while (resultSet.next()) {
+        System.out.println("username " + resultSet.getString("username")
+                + ", secretHash " + resultSet.getString("secret")
+                + ", email "     + resultSet.getString("email")
+                + ", status " + resultSet.getString("status"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (resultSet != null) {
+          resultSet.close();
+        }
+        if (ps != null){
+          ps.close();
+      }if (connection != null) {
+          connection.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
     return null;
   }
 
   @Override
   public User findByEmail(String email) {
     return null;
+  }
+
+  @Override
+  public boolean checkUser(int id) {
+    return false;
   }
 }
--- clientDb/src/main/java/it/polito/clientDb/CreateDb.java
+++ clientDb/src/main/java/it/polito/clientDb/CreateDb.java
@@ -1,5 +1,8 @@
 package it.polito.clientDb;
 
+import it.polito.server.model.User;
+import it.polito.server.model.UserDAOImpl;
+
 import java.sql.*;
 
 public class CreateDb {
@@ -13,7 +16,6 @@ public class CreateDb {
     try {
       Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db", "root", "pass");
       createDatabase(con);
-      con.close();
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -25,14 +27,14 @@ public class CreateDb {
             statement.addBatch("DROP TABLE IF EXISTS positions;");
             statement.addBatch("DROP TABLE IF EXISTS users;");
             statement.addBatch("DROP TYPE IF EXISTS user_status;");
-            statement.addBatch("CREATE TYPE user_status AS ENUM ('banned', 'approved', 'awaiting');");
+            statement.addBatch("CREATE TYPE user_status AS ENUM ('BANNED', 'APPROVED', 'AWAITING');");
 
             statement.addBatch("CREATE TABLE users (" +
                     "uid SERIAL PRIMARY KEY," +
                     "username VARCHAR(100) NOT NULL UNIQUE," +
                     "secret CHAR(60) NOT NULL," +
                     "email VARCHAR(100) NOT NULL UNIQUE," +
-                    "status user_status NOT NULL DEFAULT 'awaiting');");
+                    "status user_status NOT NULL DEFAULT 'AWAITING');");
 
             statement.addBatch("CREATE TABLE positions (" +
                     "pos_id BIGSERIAL PRIMARY KEY," +
@@ -44,18 +46,20 @@ public class CreateDb {
                     "'corrado'," +
                     "'$2a$10$dBIKrfUFTozThRxKL5bH8uZC9xOwdepJI5EXLIL0ZCz2N.BIUygi6'," +
                     "'prova@hotmail.it'," +
-                    "'approved')");
+                    "'APPROVED')");
             statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
                     "'elena'," +
                     "'$2a$10$sKW4LyhqK3D/ZEZlyJaMYuWT8Hi/EXw4wYw.V1DOQqxkxK/kHaxIO'," +
                     "'prova1@hotmail.it'," +
-                    "'approved')");
+                    "'APPROVED')");
             statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
                     "'ciccio'," +
                     "'$2a$10$zAbz3Qd3nN.sTsinoy6qYO6f6K.cGYA6lzIaiE2mgNyjhETF/SwK2'," +
                     "'prova2@hotmail.it'," +
-                    "'approved')");
+                    "'APPROVED')");
             statement.executeBatch();
+            c.close();
+            (new UserDAOImpl()).findAll();
 
         } catch (java.sql.BatchUpdateException eb) {
             throw eb.getNextException();

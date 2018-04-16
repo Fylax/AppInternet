c3e49a53161d848d9cdcc960a5e3744a5d2f31a1
Nico Caprioli <nico.caprioli@gmail.com>
2018-04-16 21:50:27 +0200

    Aggiunta condizione su foreign key e aggiunto index multiplo

--- clientDb/src/main/java/it/polito/clientDb/CreateDb.java
+++ clientDb/src/main/java/it/polito/clientDb/CreateDb.java
@@ -20,59 +20,61 @@ public class CreateDb {
 
   }
 
-    private static void createDatabase(Connection c) throws SQLException {
-      /***   USERS TABLE   ***/
-        try (Statement statement = c.createStatement()) {
-            statement.addBatch("DROP TABLE IF EXISTS positions;");
-            statement.addBatch("DROP TABLE IF EXISTS users;");
-            statement.addBatch("DROP TYPE IF EXISTS user_status;");
-            statement.addBatch("CREATE TYPE user_status AS ENUM ('BANNED', 'APPROVED', 'AWAITING');");
+  private static void createDatabase(Connection c) throws SQLException {
+    /***   USERS TABLE   ***/
+    try (Statement statement = c.createStatement()) {
+      statement.addBatch("DROP TABLE IF EXISTS positions;");
+      statement.addBatch("DROP TABLE IF EXISTS users;");
+      statement.addBatch("DROP TYPE IF EXISTS user_status;");
+      statement.addBatch("CREATE TYPE user_status AS ENUM ('BANNED', 'APPROVED', 'AWAITING');");
 
-            statement.addBatch("CREATE TABLE users (" +
-                    "uid SERIAL PRIMARY KEY," +
-                    "username VARCHAR(100) NOT NULL UNIQUE," +
-                    "secret CHAR(60) NOT NULL," +
-                    "email VARCHAR(100) NOT NULL UNIQUE," +
-                    "status user_status NOT NULL DEFAULT 'AWAITING');");
+      statement.addBatch("CREATE TABLE users (" +
+                         "uid SERIAL PRIMARY KEY," +
+                         "username VARCHAR(100) NOT NULL UNIQUE," +
+                         "secret CHAR(60) NOT NULL," +
+                         "email VARCHAR(100) NOT NULL UNIQUE," +
+                         "status user_status NOT NULL DEFAULT 'AWAITING');");
 
-            statement.addBatch("CREATE TABLE positions (" +
-                    "pos_id BIGSERIAL PRIMARY KEY," +
-                    "t_stamp timestamp NOT NULL," +
-                    "curr_location point NOT NULL," +
-                    "user_id INTEGER NOT NULL REFERENCES users(uid))");
+      statement.addBatch("CREATE TABLE positions (" +
+                         "pos_id BIGSERIAL PRIMARY KEY," +
+                         "t_stamp timestamp NOT NULL," +
+                         "curr_location point NOT NULL," +
+                         "user_id INTEGER NOT NULL REFERENCES users(uid) ON DELETE RESTRICT" +
+                         ")");
+      statement.addBatch("CREATE INDEX t_stamp_uid ON positions (user_id, t_stamp);");
 
-            statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
-                    "'corrado'," +
-                    "'$2a$10$dBIKrfUFTozThRxKL5bH8uZC9xOwdepJI5EXLIL0ZCz2N.BIUygi6'," +
-                    "'prova@hotmail.it'," +
-                    "'APPROVED')");
-            statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
-                    "'elena'," +
-                    "'$2a$10$sKW4LyhqK3D/ZEZlyJaMYuWT8Hi/EXw4wYw.V1DOQqxkxK/kHaxIO'," +
-                    "'prova1@hotmail.it'," +
-                    "'APPROVED')");
-            statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
-                    "'ciccio'," +
-                    "'$2a$10$zAbz3Qd3nN.sTsinoy6qYO6f6K.cGYA6lzIaiE2mgNyjhETF/SwK2'," +
-                    "'prova2@hotmail.it'," +
-                    "'APPROVED')");
+      statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
+                         "'corrado'," +
+                         "'$2a$10$dBIKrfUFTozThRxKL5bH8uZC9xOwdepJI5EXLIL0ZCz2N.BIUygi6'," +
+                         "'prova@hotmail.it'," +
+                         "'APPROVED')");
+      statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
+                         "'elena'," +
+                         "'$2a$10$sKW4LyhqK3D/ZEZlyJaMYuWT8Hi/EXw4wYw.V1DOQqxkxK/kHaxIO'," +
+                         "'prova1@hotmail.it'," +
+                         "'APPROVED')");
+      statement.addBatch("INSERT INTO users VALUES(DEFAULT, " +
+                         "'ciccio'," +
+                         "'$2a$10$zAbz3Qd3nN.sTsinoy6qYO6f6K.cGYA6lzIaiE2mgNyjhETF/SwK2'," +
+                         "'prova2@hotmail.it'," +
+                         "'APPROVED')");
 
-                                /*** add dummy positions ***/
-            statement.addBatch("INSERT INTO positions VALUES(DEFAULT, " +
-                    "NOW()," +
-                    "POINT(45.0649801, 7.6581405)," +
-                    "'elena')");
-            statement.addBatch("INSERT INTO positions VALUES(DEFAULT, " +
-                    "NOW() + 1," +
-                    "POINT(45.0649811, 7.6581415)," +
-                    "'ciccio')");
-            statement.executeBatch();
-        } catch (java.sql.BatchUpdateException eb) {
-            throw eb.getNextException();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+      /*** add dummy positions ***/
+      statement.addBatch("INSERT INTO positions VALUES(DEFAULT, " +
+                         "NOW()," +
+                         "POINT(45.0649801, 7.6581405)," +
+                         "'elena')");
+      statement.addBatch("INSERT INTO positions VALUES(DEFAULT, " +
+                         "NOW() + 1," +
+                         "POINT(45.0649811, 7.6581415)," +
+                         "'ciccio')");
+      statement.executeBatch();
+    } catch (java.sql.BatchUpdateException eb) {
+      throw eb.getNextException();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      e.printStackTrace();
     }
+  }
 
 }

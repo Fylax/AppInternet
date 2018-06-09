0e90128de0bf38501ec36ba3a97453ef7cedea5c
Nico Caprioli <nico.caprioli@gmail.com>
2018-06-09 16:17:00 +0200

    Tolto CSRF dall'authentication server

--- src/main/java/it/polito/ai/springserver/authorization/security/WebSecurityConfig.java
+++ src/main/java/it/polito/ai/springserver/authorization/security/WebSecurityConfig.java
@@ -5,13 +5,20 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +38,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
   @Bean
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().cors();
   }
 }
 

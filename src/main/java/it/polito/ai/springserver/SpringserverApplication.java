2e6ad544b08d03b295bb7da3f89049489a28d001
hhh01 <giuseppe.saitta01@gmail.com>
2018-09-17 10:06:58 +0200

    Fix bug client secret

--- src/main/java/it/polito/ai/springserver/SpringserverApplication.java
+++ src/main/java/it/polito/ai/springserver/SpringserverApplication.java
@@ -1,7 +1,13 @@
 package it.polito.ai.springserver;
 
+import it.polito.ai.springserver.authorization.model.OAuth2Client;
+import it.polito.ai.springserver.authorization.model.repository.OAuth2ClientRepositoryInterface;
+import it.polito.ai.springserver.authorization.model.repository.UserRepositoryInterface;
+import it.polito.ai.springserver.resource.model.repository.PositionRepositoryInterface;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -9,6 +15,18 @@ public class SpringserverApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringserverApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner init(UserRepositoryInterface userRepository,
+                         OAuth2ClientRepositoryInterface clientRepository,
+                         PositionRepositoryInterface positionRepository) {
+    return new CommandLineRunner() {
+      @Override
+      public void run(String... args) throws Exception {
+        clientRepository.save(new OAuth2Client("client", "secret"));
+      }
+    };
   }
 
 }
